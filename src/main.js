b99d3e348a0d6862596325d1b28bcddd6ffa058c
CodeGallantX <johnayobami77@proton.me>
2024-08-18 01:10:39 +0100

    .

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 function showSideBar() {
     const sidebar = document.querySelector('#sidebar');
-    sidebar.classList.remove('translate-x-full');
-    sidebar.classList.add('translate-x-0');
+    sidebar.classList.remove('translate-y-full');
+    sidebar.classList.add('translate-y-0');
   }
   
   function hideSideBar() {
     const sidebar = document.querySelector('#sidebar');
-    sidebar.classList.remove('translate-x-0');
-    sidebar.classList.add('translate-x-full');
+    sidebar.classList.remove('translate-y-0');
+    sidebar.classList.add('translate-y-full');
   }
