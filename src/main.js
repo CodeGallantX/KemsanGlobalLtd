e740e62459683bfb1c21bb5d0793d863f6284fba
CodeGallantX <johnayobami77@proton.me>
2024-08-18 19:33:50 +0100

    .

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 function showSideBar() {
-    const sidebar = document.querySelector('#sidebar');
-    sidebar.classList.remove('translate-y-full');
-    sidebar.classList.add('translate-y-0');
-  }
-  
-  function hideSideBar() {
-    const sidebar = document.querySelector('#sidebar');
-    sidebar.classList.remove('translate-y-0');
-    sidebar.classList.add('translate-y-full');
-  }
+  const sidebar = document.querySelector('#sidebar');
+  sidebar.classList.remove('-translate-y-full');
+  sidebar.classList.add('translate-y-0');
+}
+
+function hideSideBar() {
+  const sidebar = document.querySelector('#sidebar');
+  sidebar.classList.remove('translate-y-0');
+  sidebar.classList.add('-translate-y-full');
+}
